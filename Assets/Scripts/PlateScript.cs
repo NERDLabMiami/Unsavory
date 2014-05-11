1881e47decd9045cb55813bc9bce17af0bcf370b
Clay <c.ewing@miami.edu>
2014-05-11 14:46:38 -0400

    iOS matching fix
    removed LINQ orderby

--- Assets/Scripts/PlateScript.cs
+++ Assets/Scripts/PlateScript.cs
@@ -10,18 +10,22 @@ public class PlateScript : MonoBehaviour {
 	public GameObject strikeCounter;
 
 	private void OnMouseDown() {
-
+		Debug.Log("Detected Mouse Down");
 		GameObject[] current_recipes = GameObject.FindGameObjectsWithTag("Recipe");
 		bool matched = false;
 		int matchedOrderIndex = -1;
 		for (int i = 0; i < current_recipes.Length; i++) {
-			if (ScrambledEquals(current_recipes[i].GetComponent<RecipeScript>().ingredients,ingredients)) {
-				//ADD JUICE!
+
+//			if (ScrambledGOEquals(current_recipes[i].GetComponent<RecipeScript>().ingredients,ingredients)) {
+			if (doOrdersMatch(current_recipes[i].GetComponent<RecipeScript>().ingredients,ingredients)) {
+
+			//ADD JUICE!
 				//found a match
 				matched = true;
 				matchedOrderIndex = i;
 				break;
 			}
+
 		}
 
 
@@ -39,20 +43,32 @@ public class PlateScript : MonoBehaviour {
 			Destroy (foodOnTheTable[j]);
 		}
 
-		//create another order
-//		currentOrder.GetComponent<CurrentOrderScript>().Spawn();
 	}
 
-	public static bool ScrambledEquals<T>(IEnumerable<T> list1, IEnumerable<T> list2) {
-		var cnt = new Dictionary<T, int>();
-		foreach (T s in list1) {
+	public bool doOrdersMatch(List<GameObject> list1, List<GameObject> list2) {
+		if (list1.Count != list2.Count) {
+			return false;
+		}
+		else {
+			for (int i = 0; i < list1.Count; i++) {
+				if (!list2.Contains(list1[i]) | !list1.Contains(list2[i])) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static bool ScrambledGOEquals<GameObject>(List<GameObject> list1, List<GameObject> list2) {
+		var cnt = new Dictionary<GameObject, int>();
+		foreach (GameObject s in list1) {
 			if (cnt.ContainsKey(s)) {
 				cnt[s]++;
 			} else {
 				cnt.Add(s, 1);
 			}
 		}
-		foreach (T s in list2) {
+		foreach (GameObject s in list2) {
 			if (cnt.ContainsKey(s)) {
 				cnt[s]--;
 			} else {
