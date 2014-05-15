b446e2beb485353d3cb503927088ac1eb1193a67
Clay <c.ewing@miami.edu>
2014-05-14 20:24:16 -0400

    Home Scene Buttons
    Health decreases when working
    Buying drugs decreases money
    Staying home increases health, skips next level

--- Assets/Scripts/BuyDrugsScript.cs
+++ Assets/Scripts/BuyDrugsScript.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class BuyDrugsScript : MonoBehaviour {
+	public float costOfDrugs = 5.99f;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnMouseDown() {
+
+		float money = PlayerPrefs.GetFloat("money");
+		if (money - costOfDrugs >= 0) {
+			money -= costOfDrugs;
+			PlayerPrefs.SetFloat("money",money);
+		}
+		//TODO: figure out health change here vs. flag in the game round
 	}
 }
--- Assets/Scripts/WorkScript.cs
+++ Assets/Scripts/WorkScript.cs
@@ -2,14 +2,13 @@
 using System.Collections;
 
 public class WorkScript : MonoBehaviour {
+	private float health;
+	public float decreaseHealthMinimum = 1;
+	public float decreaseHealthMaximum = 2;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnMouseDown() {
+		health = PlayerPrefs.GetFloat("health");
+		health -= Random.Range (decreaseHealthMaximum, decreaseHealthMinimum);
+		PlayerPrefs.SetFloat("health",health);
 	}
 }
