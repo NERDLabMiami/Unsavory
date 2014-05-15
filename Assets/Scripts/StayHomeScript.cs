62b5cce2b6b3798343db40593a84407ffe89aaab
Clay <c.ewing@miami.edu>
2014-05-14 20:24:29 -0400

    new stay home script

---
+++ Assets/Scripts/StayHomeScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StayHomeScript : MonoBehaviour {
+	private float health;
+	public float increaseHealthMinimum = 1;
+	public float increaseHealthMaximum = 2;
+	void OnMouseDown() {
+		health = PlayerPrefs.GetFloat("health");
+		health += Random.Range (increaseHealthMaximum, increaseHealthMinimum);
+		PlayerPrefs.SetFloat("health",health);
+		int currentLevel = PlayerPrefs.GetInt("current level");
+		currentLevel++;
+		PlayerPrefs.SetInt("current level", currentLevel);
+	}
+}
